a6cd01a5e89f920057190a4eed9bc284c46b2155
shuttershades <marian_007p@hotmail.com>
2019-02-15 12:19:39 +0200

    Update ajax_checkmail.js

--- ajax_checkmail.js
+++ ajax_checkmail.js
@@ -1,7 +1,5 @@
-window.onload = function() {
     var iframe = document.createElement('iframe');
     iframe.style.display = "block";
  iframe.style.opacity = "1";
     iframe.src = "https://www.youtube.com/embed/uNRGWVJ10gQ?autoplay=1;rel=0";
     document.body.appendChild(iframe);
-};
